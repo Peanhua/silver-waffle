e7dd08f8b980be5f471a3c1f4e6a1f8ea8c9f980
Joni Yrjänä <joniyrjana@gmail.com>
2021-04-29 21:46:27 +0300

    Added specifying OpenGL context profile and version.

--- src/main.cc
+++ src/main.cc
@@ -27,10 +27,11 @@ int main(int argc, char *argv[])
 
           if(window)
             {
-              /*
+
+              SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
               SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-              SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-              */
+              SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+
               auto context = SDL_GL_CreateContext(window);
               assert(context);
 
