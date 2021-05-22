b34472393c658c1ea81819f35d6618ed9e12f807
Joni Yrjänä <joniyrjana@gmail.com>
2021-05-23 01:49:29 +0300

    Added borders around the spaceship health status meter.

--- src/WidgetSpaceshipStatus.cc
+++ src/WidgetSpaceshipStatus.cc
@@ -8,30 +8,58 @@ WidgetSpaceshipStatus::WidgetSpaceshipStatus(Widget * parent, const glm::ivec2 &
   : Widget(parent, position, size),
     _spaceship(nullptr)
 {
-  _meter = new Mesh(Mesh::OPTION_ELEMENT | Mesh::OPTION_COLOR | Mesh::OPTION_BLEND);
-  assert(_meter);
-  _meter->SetShaderProgram(AssetLoader->LoadShaderProgram("Generic-Color"));
+  {
+    _meter = new Mesh(Mesh::OPTION_ELEMENT | Mesh::OPTION_COLOR | Mesh::OPTION_BLEND);
+    assert(_meter);
+    _meter->SetShaderProgram(AssetLoader->LoadShaderProgram("Generic-Color"));
 
-  std::vector<glm::vec3> vertices {
-    glm::vec3(1, 1, 0), 
-    glm::vec3(1, 0, 0), 
-    glm::vec3(0, 0, 0),
-    glm::vec3(0, 1, 0),
-  };
+    std::vector<glm::vec3> vertices {
+      glm::vec3(1, 1, 0), 
+      glm::vec3(1, 0, 0), 
+      glm::vec3(0, 0, 0),
+      glm::vec3(0, 1, 0),
+    };
   
-  for(auto v : vertices)
-    {
-      _meter->AddVertex(glm::vec3(v.xy(), 0));
-      _meter->AddColor(glm::vec3(0, 1, 0));
-    }
+    for(auto v : vertices)
+      {
+        _meter->AddVertex(glm::vec3(v.xy(), 0));
+        _meter->AddColor(glm::vec3(0, 1, 0));
+      }
 
-  std::vector<unsigned int> indices {
-    0, 1, 3,
-    1, 2, 3
-  };
-  for(auto i : indices)
-    _meter->AddElement(i);
+    std::vector<unsigned int> indices {
+      0, 1, 3,
+      1, 2, 3
+    };
+    for(auto i : indices)
+      _meter->AddElement(i);
+  }
 
+
+  {
+    auto borders = new Mesh(Mesh::OPTION_COLOR | Mesh::OPTION_BLEND);
+    assert(borders);
+    borders->SetShaderProgram(AssetLoader->LoadShaderProgram("Generic-Color"));
+    borders->SetPrimitiveType(GL_LINES);
+  
+    std::vector<glm::vec3> vertices {
+      glm::vec3(0, 0, 0),
+      glm::vec3(1, 0, 0),
+      glm::vec3(1, 0, 0),
+      glm::vec3(1, 1, 0),
+      glm::vec3(1, 1, 0),
+      glm::vec3(0, 1, 0),
+      glm::vec3(0, 1, 0),
+      glm::vec3(0, 0, 0),
+    };
+    for(auto v : vertices)
+      {
+        borders->AddVertex(glm::vec3(v.xy(), v.z));
+        borders->AddColor(glm::vec3(0, 0.5, 1));
+      }
+  
+    _meter->AddChild(borders);
+  }
+  
   _meter->UpdateGPU();
 }
 
